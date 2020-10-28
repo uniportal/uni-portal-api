b8ba2c0db066a90e00b13ff0493607f7a97840dd
victor_jsantos <victorjsantos240301@gmail.com>
2020-10-28 03:10:57 -0300

    first commit

---
+++ src/main/java/br/com/uniportal/UniPortalApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.uniportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniPortalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniPortalApiApplication.class, args);
+	}
+
+}
